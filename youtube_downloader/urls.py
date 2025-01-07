2c534e5c1dcb0463d6586040697e07269b0e5777
Ernest Nyakundi <nyakundi.earnest@gmail.com>
2025-01-07 20:01:40 +0300

    Resolve merge conflict in apps.py

--- youtube_downloader/urls.py
+++ youtube_downloader/urls.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.urls import path, include
 from django.shortcuts import redirect
 from . import views
@@ -7,13 +6,4 @@ urlpatterns = [
     path('video-download/', views.DownloadView.as_view(), name='download_video'),
     path('', lambda request: redirect('download_video')),  # Redirect root URL to the download view
 ]
-=======
-from django.urls import path, include
-from django.shortcuts import redirect
-from . import views
 
-urlpatterns = [
-    path('video-download/', views.DownloadView.as_view(), name='download_video'),
-    path('', lambda request: redirect('download_video')),  # Redirect root URL to the download view
-]
->>>>>>> bd74355 (Initial commit with virtual environment and requirements)
